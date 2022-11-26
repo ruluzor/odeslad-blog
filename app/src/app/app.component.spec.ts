8b7075dde1671ccddd656f7612541a88c818bcb5
Rubén Lucas <rubenlucas@gmail.com>
2022-11-26 20:02:09 +0100

    Remove angular content and update app tests

--- app/src/app/app.component.spec.ts
+++ app/src/app/app.component.spec.ts
@@ -20,13 +20,13 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'app'`, () => {
+  it(`should have as title 'Blog'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('Blog');
   });
 
-  it('should render title', () => {
+  xit('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
