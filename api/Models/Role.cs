864bb3fafa98c0f9b6ce38c1fdf9cc5ba50753df
Rubén Lucas <rubenlucas@gmail.com>
2022-11-27 17:39:28 +0100

    Add DbContext context, models and refactor
    Add user model
    
    Update api .gitignore
    
    Refactor context

---
+++ api/Models/Role.cs
@@ -0,0 +1,7 @@
+namespace Models;
+
+public enum Role
+{
+    Admin,
+    User
+}
---
+++ api/Repositories/Context.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Repositories;
+
+public class Context : DbContext
+{
+    protected readonly IConfiguration Configuration;
+
+    public Context(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseSqlServer(Configuration.GetConnectionString("Default"));
+    }
+
+    public DbSet<Models.User> Users { get; set; }
+}
