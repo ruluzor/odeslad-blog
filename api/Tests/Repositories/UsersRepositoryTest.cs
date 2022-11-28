c30f2e6cacad222039b920357f6143eb29387728
Rubén Lucas <rubenlucas@gmail.com>
2022-11-28 09:03:32 +0100

    Create users repository test

---
+++ api/Tests/Repositories/UsersRepositoryTest.cs
@@ -0,0 +1,38 @@
+using api.Repositories;
+using Xunit;
+
+namespace tests.Repositories
+{
+    public class UsersRepositoryTest
+    {
+        [Fact]
+        public void ShouldGetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void ShouldGetById()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void ShouldCreate()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void ShouldUpdate()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void ShouldDelete()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
